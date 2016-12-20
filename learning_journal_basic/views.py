47b14ceeebb9e6645d96c29eb1621100521f8fa8
Maelle Vance <maellevance@gmail.com>
2016-12-19 17:46:51 -0800

    added views, routes and templates

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -3,15 +3,37 @@
 from pyramid.response import Response
 import os
 
+
 HERE = os.path.dirname(__file__)
 
 
-def home_page(request):
+def home_list(request):
     """View for the home page."""
-    imported_text = open(os.path.join(HERE, "data/" 'sample.html')).read()
+    imported_text = open(os.path.join(HERE, 'templates/index.html')).read()
+    return Response(imported_text)
+
+
+def detail(request):
+    """View for the detail page."""
+    imported_text = open(os.path.join(HERE, 'templates/post_detail.html')).read()
+    return Response(imported_text)
+
+
+def create(request):
+    """View for create page."""
+    imported_text = open(os.path.join(HERE, 'templates/new_entry.html')).read()
+    return Response(imported_text)
+
+
+def update(request):
+    """View for update page."""
+    imported_text = open(os.path.join(HERE, 'templates/edit_entry.html')).read()
     return Response(imported_text)
 
 
 def includeme(config):
     """Include me function."""
-    config.add_view(home_page, route_name='home')
+    config.add_view(home_list, route_name='home')
+    config.add_view(detail, route_name='detail')
+    config.add_view(create, route_name='create')
+    config.add_view(update, route_name='update')
