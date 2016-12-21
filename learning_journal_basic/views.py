dfeb3735bb2b4ffed17087f062cfe4c3ff7ecb13
Amos Boldor <amosboldor@gmail.com>
2016-12-20 18:39:39 -0800

    removed corpse code and edit journal entries

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -3,7 +3,7 @@
 from pyramid.view import view_config
 ENTRIES = [
     {
-        "title": "Monday 12/19: Day 12",
+        "title": "It's Monday Dude",
         "id": 1,
         "published_on": "Dec 20, 2016",
         "body": """Today we got to learn about the python framework pyramid and it was not that hard to setup just tedious.
@@ -12,26 +12,23 @@ ENTRIES = [
         """
     },
     {
-        "title": "Entry 2",
+        "title": "It's Tuesday Dude",
         "id": 2,
-        "creation_date": "Dec 21, 2016",
+        "published_on": "Dec 21, 2016",
         "body": "I learned some stuff about some other stuff. Also Maelle is an awesome partner! Yay her!"
-    },
+    }
 ]
 
 
 @view_config(route_name="home", renderer="templates/index.jinja2")
 def home_list(request):
     """View for the home page."""
-    # imported_text = open(os.path.join(HERE, 'templates/index.html')).read()
-    # # return Response(imported_text)
     return {"posts": ENTRIES}
 
 
 @view_config(route_name="detail", renderer="templates/post_detail.jinja2")
 def detail(request):
     """View for the detail page."""
-    # request.matchdict.['id']
     return {"post": ENTRIES[int(request.matchdict['id']) - 1]}
 
 
@@ -44,4 +41,3 @@ def create(request):
 @view_config(route_name="update", renderer="template")
 def update(request):
     """View for update page."""
-
