5fc56985cc4b861edfada1debf1a84928d81af48
Amos Boldor <amosboldor@gmail.com>
2016-12-20 19:41:21 -0800

    add docstrings to __init__

--- learning_journal_basic/__init__.py
+++ learning_journal_basic/__init__.py
@@ -1,9 +1,9 @@
+"""Config page for pyramid app."""
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """Function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include(".routes")
